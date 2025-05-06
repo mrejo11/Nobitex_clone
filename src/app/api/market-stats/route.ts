ac793106cffc1d5ab638a717e9494b6e5f817207
mehdi <mreko95@protonmail.com>
2025-05-06 10:12:30 +0330

    apiBugFix

--- src/app/api/market-stats/route.ts
+++ src/app/api/market-stats/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch("https://api.nobitex.ir/market/stats", {
+    const response = await fetch("https://nobitex-clone.netlify.app", {
       cache: "no-store",
     });
     if (!response.ok) throw new Error("Failed to fetch data");
